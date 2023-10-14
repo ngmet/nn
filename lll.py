5995f48e2464ff2fd2fa83071b4511a539f17e59
ngmet <143947759+ngmet@users.noreply.github.com>
2023-10-14 18:45:07 +0600

    Create lll.py
    vn

---
+++ lll.py
@@ -0,0 +1,38 @@
+
+HTML КОД
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>дракон</h1>
+    <p>протев</p>
+    <h2>лиса</h2>
+             <ul>
+                   <li>пабеда за драконам</li>
+                   <li>лиса лох</li>
+                   <li>лиса збеслас</li>
+                   <li>и унечтожла дракона</li>
+             </ul>
+     <img src="https://avatars.mds.yandex.net/i?id=140c3fa7e858be26b5a2baa77e740d3468950733-9065974-images-thumbs&n=13" alt="лиса">
+</body>
+</html>
+
+
+CSS КОД
+
+body {
+    font-family: Arial,  sans-serif; /* Семейство шрифтов */
+    font-size: 22px; /* Размер основного шрифта в <p></p>  */
+    background-color: #131212f8; /* Цвет фона веб-страницы */
+    color: #d4cccc; /* Цвет основного текста */ 
+}
+h1 {
+    color: #f80a0a; /* Цвет заголовка */
+    font-size: 19px; /* Размер шрифта в пунктах */
+    font-family: Georgia, serif; /* Семейство шрифтов */
+}
